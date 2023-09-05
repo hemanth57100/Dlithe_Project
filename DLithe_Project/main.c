9efaf32a825377a1cd27ab5876863ea65ee05ade
Prithesh <113179898+prithesh17@users.noreply.github.com>
2023-09-05 23:18:39 +0530

    Update main.c

--- DLithe_Project/main.c
+++ DLithe_Project/main.c
@@ -3,9 +3,14 @@
 #include "compute.c"
 
 int main() {
+	// Initialize the curses library
 	initscr();
+	// Initialize color support
 	start_color();
+	// Hide the cursor
 	curs_set(FALSE);
+	 // Call a function to start the menu or user interface
 	startMenu();
+	// Clean up and close the curses library
 	endwin();
 }
